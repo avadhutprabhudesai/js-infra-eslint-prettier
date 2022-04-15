b298d038f02db1253c5d96a205bcc2aa9d251f7a
Avadhut Prabhudesai <avadhutprabhudesai23@gmail.com>
2022-04-15 12:50:14 +0530

    eslint and prettier explored.

---
+++ App.jsx
@@ -0,0 +1,5 @@
+import React, { useState } from "react";
+
+function App({ id }) {
+  const [first, setfirst] = useState();
+}
---
+++ index.ts
@@ -0,0 +1,4 @@
+const add = (x: number, y: number): number => {
+  return x + y;
+};
+add(1, 2);
